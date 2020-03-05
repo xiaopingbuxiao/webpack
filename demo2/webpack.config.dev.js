3d127f122b600bf3bc1310d3231a70b78868b0e4
xiaopingbuxiao <1210774823@qq.com>
2020-03-05 22:17:49 +0800

    demo

---
+++ demo2/webpack.config.dev.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry:{
+    index:'./src/index.js'
+  },
+  output:{
+    filename: '[name].js',
+    path: path.join(__dirname,'./dist'),
+    publicPath:'/'
+  },
+  mode:'development',
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        use:'babel-loader'
+      },
+      {
+        test:/\.(scss|css)$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin(),
+    new htmlWebpackPlugin({
+      title:'hellow webpack',
+      template:'./index.html',
+      // chunks:['index']
+    })
+  ]
+}
