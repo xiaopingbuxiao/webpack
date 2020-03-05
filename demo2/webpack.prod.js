4151ac1c043c8c1fdc22c163553cc2152389c67b
xiaopingbuxiao <1210774823@qq.com>
2020-03-05 23:01:44 +0800

    demo2

---
+++ demo2/webpack.prod.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const optimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]_[chunkhash:8].js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          // 'style-loader',
+          miniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              name: 'img/[name][hash:8].[ext]'
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new miniCssExtractPlugin({
+      filename: '[name][contenthash:8].css',
+    }),
+    new optimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano')
+    }),
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: false
+      }
+    }),
+  ]
+}
