4697f7aff9a4dd8e05fa10cb36ffc8414967ef70
xiaopingbuxiao <1210774823@qq.com>
2020-03-11 00:03:32 +0800

    懒加载js优化首屏 动态import

--- splitChunks/src/search/index.js
+++ splitChunks/src/search/index.js
@@ -8,10 +8,30 @@ import { a } from './tree-shaking'
 var str = a()
 console.log(common())
 class Search extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent() {
+    import('./Text.jsx').then((Text) => {
+      console.log(Text.default)
+      this.setState({
+        Text: Text.default
+      })
+    })
+  }
   render() {
+    const { Text } = this.state
     return (
-      <div className='search-text'>
-        <img src={logo} />
+      <div className='search-Text'>
+        {
+          Text ? <Text></Text> : null
+        }
+        <img src={logo} onClick={this.loadComponent.bind(this)} />
         <span>搜索页面的html{str}</span>
       </div>
     )
